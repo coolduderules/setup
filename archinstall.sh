e5aed2742f0d67dfeb8179a8bfc9fb362f2bccbc
Thiago Perrotta <tbperrotta@gmail.com>
2025-01-19 04:56:48 +0000

    Jan 18, 2025, 11:56 PM

--- archinstall.sh
+++ archinstall.sh
@@ -50,13 +50,13 @@ if [ -v ARCH_CFG ]; then
     echo "Config read from \$ARCH_CFG environment variable"
 else
     read -p "PATH TO CONFIG FILE: " ARCH_CFG
-    export ARCH_CFG=$ARCH_CFG
+    cat $ARCH_CFG >> $CONFIG_FILE
 fi
 if [ -v ARCH_CREDS ]; then
     echo "Config read from \$ARCH_CREDS environment variable"
 else
     read -p "PATH TO CREDENTIAL FILE: " ARCH_CREDS
-    export ARCH_CREDS=$ARCH_CREDS
+    cat $ARCH_CREDS >> $CONFIG_FILE
 fi
 
 lin="LIN$suff"
