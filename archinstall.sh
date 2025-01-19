b70032f95fd3c27977a20473f468a97ec5edd599
Thiago Perrotta <tbperrotta@gmail.com>
2025-01-19 03:07:02 +0000

    Jan 18, 2025, 10:07 PM

--- archinstall.sh
+++ archinstall.sh
@@ -10,7 +10,7 @@ fi
 # Check root privileges
 if [ $EUID != 0 ]; then
     if [ "$PWD" != "/Users/jason/Github/setup" ]; then
-        sudo "$0" "$@"
+        sudo -E "$0" "$@"
         exit $?
     fi
 fi
